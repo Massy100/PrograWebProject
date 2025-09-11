d8006348a287692e4a2d2ece9b7dd716d760647a
GreciaFuent <greciafuentes307@gmail.com>
2025-09-11 17:23:15 -0600

    feat(header): create the component header

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,17 @@
-export default async function Home() {
+import Header from '../components/Header';
+
+export default function Home() {
+  const isLoggedIn = true;
+  const marketOpen = false;
+  const totalAmount = 100;
 
   return (
     <main>
+      <Header
+        isLoggedIn={isLoggedIn}
+        marketOpen={marketOpen}
+        totalAmount={totalAmount}
+      />
       <h1>Estoy en la pagina principal</h1>
     </main>
   );
