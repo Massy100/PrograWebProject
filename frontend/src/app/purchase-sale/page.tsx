c68748adf32ca90c30a3d2b30e75b28d64246651
GreciaFuent <greciafuentes307@gmail.com>
2025-09-21 12:16:18 -0600

    feat(Transactions): create the transactions page, with the styles and the conections of the componentes

--- frontend/src/app/purchase-sale/page.tsx
+++ frontend/src/app/purchase-sale/page.tsx
@@ -1,3 +1,86 @@
+import FolderCard from "@/components/portfolioAccesePS";
+import SearchResults from "@/components/searchResults";
+import './purchaseSale.css'
+import TxTable from "@/components/tableTrasactions";
+import KpiCard from "@/components/KpiCard";
+import ChartSwitcher from "@/components/ChartSwitcher";
+import PortfolioList from "@/components/PortfolioList";
+
+
 export default function Purchasesale() {
-  return <h1> --- Hola estoy en Purchasesale--</h1>;
+
+const rows = [
+  {
+    transaction_type: 'sell',
+    stock: 'Microsoft',
+    code: 'MSFT',
+    created_at: '2025-09-16',
+    quantity: 3,
+    unit_price: 410.25,
+    total_amount: 1230.75,
+    is_active: true,
+  },
+  {
+    transaction_type: 'buy',
+    stock: 'NVIDIA',
+    code: 'NVDA',
+    created_at: '2025-09-14',
+    quantity: 2,
+    unit_price: 460,
+    total_amount: -920,
+    is_active: false,
+  },
+];
+
+
+  return (
+    <div className="div-purchaseSale">
+      <SearchResults
+        headerProps={{ isLoggedIn: true, marketOpen: true, totalAmount: 100 }}
+        title="Resultados de la búsqueda"
+      />
+
+      <div className="info-purchaseSale">
+
+        <div className="summary-div-transactions">
+          <KpiCard
+            title="Earned Total"
+            value="700"
+            format="money"
+            dark
+          />
+          <KpiCard
+            title="Invested Total"
+            value="700"
+            format="money"
+          />
+          <KpiCard
+            title="Stocks Buy"
+            value="700"
+            format="number"
+          />
+          <KpiCard
+            title="Stocks Buy"
+            value="700"
+            format="number"
+          />
+        </div>
+
+        <div className="div-grafics-portfolio">
+          <div className="div-grafics">
+            <ChartSwitcher />
+          </div>
+          <div className="div-portfolio">
+            <PortfolioList/>
+          </div>
+        </div>
+
+
+        <h3 className="title_transactions">Transactions</h3>
+        <div className="div-transactions">
+          <TxTable rows={rows} />
+        </div>
+      </div>
+    </div>
+  )
 }
