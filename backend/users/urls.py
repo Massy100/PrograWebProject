c34184cb0faa6ccb93e7b216473f7045219ff402
CCVela1017 <carlosvelaeduardo@gmail.com>
2025-09-26 21:24:27 -0600

    feat(grantor): created permit grantor and userauth requests endpoints

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -8,8 +8,20 @@ from .views import (
     StockManagementView
 )
 
+from .auth_views.admin_request_views import AdminPermsReqViewSet
+
 router = SimpleRouter()
 router.register(r'', UserViewSet, basename='user')
+router.register(r'/admin-requests', AdminPermsReqViewSet, basename='admin-requests')
+
+"""
+administrator/admin-requests/pending/  -> Listar solicitudes pendientes
+administrator/admin-requests/approved/ -> Listar solicitudes aprobadas
+administrator/admin-requests/rejected/ -> Listar solicitudes rechazadas
+administrator/admin-requests/{id}/approve/ -> Aprobar solicitud
+administrator/admin-requests/{id}/reject/ -> Rechazar solicitud
+client/admin-requests/ -> Crear nueva solicitud
+"""
 
 permission_urls = [
     # URLS DE PRUEBA DE ROLES Y PERMISOS (para testing)
@@ -17,6 +29,8 @@ permission_urls = [
     path('client/transactions/', TransactionHistoryView.as_view(), name='transaction-history'),
     path('administrator/users/', user_management, name='user-management'),
     path('administrator/stocks/', StockManagementView.as_view(), name='stock-management'),
+
+
 ]
 
 urlpatterns = router.urls + permission_urls
