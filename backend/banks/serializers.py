9695baef197dc20269ae9f00eb7b63a0f2abe530
CCVela1017 <carlosvelaeduardo@gmail.com>
2025-09-30 18:25:03 -0600

    feat: added initial banks data serializers

---
+++ backend/banks/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Bank, FundsTransfer
+
+class BankSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bank
+        fields = '__all__'
+
+class FundsTransferSerializer(serializers.ModelSerializer):
+    bank = BankSerializer(read_only=True)
+    user_name = serializers.CharField(source='user.username', read_only=True)
+
+    class Meta:
+        model = FundsTransfer
+        fields = ['id', 'user', 'user_name', 'bank', 'amount', 'transfer_date']
+        
