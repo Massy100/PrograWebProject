c963336336f6cdd6c44a14316126eab62b95e472
brandonvasm <brandonmonzon2005@gmail.com>
2025-10-04 18:25:09 -0600

    feat(login): update fields, validations, and switch language to English

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -3,6 +3,10 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import "../styles/login.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { PhoneNumberUtil } from "google-libphonenumber";
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
 
 type LoginProps = {
   open: boolean;
@@ -10,10 +14,20 @@ type LoginProps = {
 };
 
 export default function Login({ open, onClose }: LoginProps) {
-  const [showLogin, setShowLogin] = useState(true);
   const router = useRouter();
+  const phoneUtil = PhoneNumberUtil.getInstance();
+
+  const [showLogin, setShowLogin] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);           
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false); 
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [phoneError, setPhoneError] = useState(""); 
+  const [hasReferral, setHasReferral] = useState(false);
+  const [referralError, setReferralError] = useState("");
+  const [phoneFull, setPhoneFull] = useState("");
 
   if (!open) return null; // <-- si no está abierto, no renderiza nada
+
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -46,15 +60,41 @@ export default function Login({ open, onClose }: LoginProps) {
   const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const data = {
+    const password = String(formData.get("password"));
+    const confirmPassword = String(formData.get("confirmPassword"));
+
+    if (password !== confirmPassword) {
+      setPasswordError("Passwords must match");
+      return;
+    } else {
+      setPasswordError("");
+    }
+
+    if (!phoneFull) {
+      setPhoneError("Phone number is required");
+      return;
+    } else {
+      setPhoneError("")
+    }
+
+    const referralCode = String(formData.get("referred_code")).trim();
+    if (hasReferral && !referralCode) {
+      setReferralError("Referral code is required");
+      return;
+    } else {
+      setReferralError("");
+    }
+
+
+    try {
+      const data = {
       username: formData.get("name"),
       email: formData.get("email"),
       password: formData.get("password"),
       phone: formData.get("phone"),
       referred_code: formData.get("referred_code"),
-    };
-
-    try {
+      };
+    
       const res = await fetch("http://localhost:8000/api/users/register/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -74,36 +114,54 @@ export default function Login({ open, onClose }: LoginProps) {
   };
 
   return (
-    <div className="modalOverlay" onClick={onClose}>
+    <div className="modalOverlay"  onClick={() => { setPhoneFull("");  setPhoneError(""); onClose?.(); }}>
       <div className="modal" onClick={(e) => e.stopPropagation()}>
-        <button className="closeButton" aria-label="Cerrar" onClick={onClose}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"></path></g></svg></button>
+        <button className="closeButton" aria-label="Cerrar" onClick={() => { setPhoneFull("");  setPhoneError("");onClose?.(); }}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"></path></g></svg></button>
 
         {showLogin ? (
           <>
             <h2 className="modalTitle">Login</h2>
             <form className="form" onSubmit={handleLogin}>
-              <input type="email" name="email" placeholder="Email" className="input" required />
-              <input type="password" name="password" placeholder="Contraseña" className="input" required />
-              <button type="submit" className="submitButton">Entrar</button>
+              <input type="email" name="email" placeholder="Email or User" className="input" required />
+              <input type="password" name="password" placeholder="Password" className="input" required />
+              <button type="submit" className="submitButton">Login</button>
             </form>
             <p className="switchText">
-              ¿No tienes cuenta?{" "}
+                Don't have an account?{" "}
               <span className="switchLink" onClick={() => setShowLogin(false)}>Regístrate</span>
             </p>
           </>
         ) : (
           <>
-            <h2 className="modalTitle">Registro</h2>
+            <h2 className="modalTitle">Register</h2>
             <form className="form" onSubmit={handleRegister}>
-              <input type="text" name="name" placeholder="Nombre" className="input" required />
+              <input type="text" name="name" placeholder="User" className="input" required />
               <input type="email" name="email" placeholder="Email" className="input" required />
-              <input type="password" name="password" placeholder="Contraseña" className="input" required />
-              <input type="tel" name="phone" placeholder="Teléfono" className="input" />
-              <input type="text" name="referred_code" placeholder="Código de referido" className="input" />
-              <button type="submit" className="submitButton">Crear cuenta</button>
+               <div className="passwordContainer">
+                <input type={showPassword ? "text" : "password"} name="password" placeholder="Password" className="input" required />
+                <button type="button" className="eyeButton" onClick={() => setShowPassword(!showPassword)}>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </div>
+              <div className="passwordContainer">
+                <input type={showConfirmPassword ? "text" : "password"} name="confirmPassword" placeholder="Confirm Password" className="input" required />
+                <button type="button" className="eyeButton" onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
+                  {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </div>
+              {passwordError && <p className="errorText">{passwordError}</p>}
+              <div className="phoneContainer">
+                <PhoneInput value={phoneFull} onChange={(value) => setPhoneFull(value)} inputClass="input" enableSearch placeholder="Phone" />
+              </div>
+              {phoneError && <p className="errorText2">{phoneError}</p>}
+
+              <input type="checkbox" checked={hasReferral} onChange={() => setHasReferral(!hasReferral)} /> I have a referral code
+              {hasReferral && <input type="text" name="referred_code" placeholder="Referral Code" className="input" />}
+              {referralError && <p className="errorText">{referralError}</p>}
+              <button type="submit" className="submitButton">Create Account</button>
             </form>
             <p className="switchText">
-              ¿Ya tienes cuenta?{" "}
+               Already have an account?{" "}
               <span className="switchLink" onClick={() => setShowLogin(true)}>Login</span>
             </p>
           </>
