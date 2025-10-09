6a76ac7f9752e80c48d15547242ade124c4390a8
CCVela1017 <carlosvelaeduardo@gmail.com>
2025-10-08 20:05:33 -0600

    feat(prices): created price and total investment of model Investment

--- backend/portfolio/models.py
+++ backend/portfolio/models.py
@@ -16,5 +16,7 @@ class Investment(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    average_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    total_invested = models.DecimalField(max_digits=12, decimal_places=2, default=0)
     purchased_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
