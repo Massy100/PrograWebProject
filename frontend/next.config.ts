059ecdcd26d2a59628beea5b2ebb83245d969ae5
Massy100 <massielle.coti@gmail.com>
2025-11-03 09:08:12 -0600

    fix: disable turbopack and change output mode to resolve not-found error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    turbo: undefined
+  },
+  
+  output: 'standalone',
+  
   eslint: {
     ignoreDuringBuilds: true,
   },
